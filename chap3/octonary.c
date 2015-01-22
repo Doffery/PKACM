8b53452e2d7e6dbc1ce086e0bb460d3550692a80
doffery <doffery20@gmail.com>
2015-01-22 22:07:48 +0800

    rectify

--- chap3/octonary.c
+++ chap3/octonary.c
@@ -1,37 +1,47 @@
 #include<stdio.h>
 #include<string.h>
 #include<math.h>
+#include<memory.h>
 
+//1000 per entry
+int result[21];
+void Multiply125() {
+	int i;
+	int carry = 0;
+	for(i = 0; i < 20; ++i) {
+		result[i] *= 125;
+		result[i] += carry;
+		carry = result[i] / 1000;
+		if(carry) {
+			result[i] = result[i] % 1000;
+		}
+	}
+}
 
 int main(void) {
 	freopen("in.txt", "r", stdin);
 	freopen("out.txt", "w", stdout);
 
-	char oin[16], dout[44]; dout[0] = '0'; dout[1] = '.';
-	long long res;
+	char oin[16];
+
 	while(scanf("%s", oin) != EOF) {
-		res = 0;
+		memset(result, 0, sizeof(result));
+
 		int len = strlen(oin);
-		res = (oin[len - 1] - '0') * 125;
+		result[0] = (oin[len - 1] - '0') * 125;
 		int i = len - 2;
 		for(; i >= 2; --i) {
 			int j = oin[i] - '0';
 			int t = 3*(len - i - 1);
-			//res += j * (pow(10.0, t)) * 125;
-			res = (res + j * (pow(10.0, t))) * 125;
+			result[len - i - 1] += j;
+			Multiply125();
+			
 		}
-		long long rt = res;
-		int x = res % 10;
-		int doutIndex = 2 + 3 * (len - 2);
-		dout[doutIndex--] = '\0';
-		while (rt != 0) {
-			rt = rt / 10;
-			dout[doutIndex--] = x + '0';
-			x = rt % 10;
+		printf("%s [8] = 0.", oin);
+		for(i = len - 3; i >= 0; --i) {
+			printf("%03d", result[i]);
 		}
-		while(doutIndex > 1)
-			dout[doutIndex--] = '0';
-		printf("%s\n", dout);
+		printf(" [10]\n");
 	}
 	return 0;
 }
